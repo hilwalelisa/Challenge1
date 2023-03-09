9a32e10df73e3e456d51d83960a4831ded9169a0
hilwalelisa <127275322+hilwalelisa@users.noreply.github.com>
2023-03-09 13:29:25 +0700

    Update main.go

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ import "fmt"
 
 func main() {
 	i := 21
-    fmt.Printf("%d\n", i)
-    fmt.Printf("%T\n", i)
-    fmt.Printf("%%\n")
-    j := true
-    fmt.Printf("%t\n\n", j)
-    fmt.Printf("%b \n", i)
-    fmt.Printf("%c \n", '\u042F')
-    fmt.Printf("%d \n", i)       
+   	fmt.Printf("%d\n", i)
+    	fmt.Printf("%T\n", i)
+    	fmt.Printf("%%\n")
+    	j := true
+    	fmt.Printf("%t\n\n", j)
+    	fmt.Printf("%b \n", i)
+    	fmt.Printf("%c \n", '\u042F')
+    	fmt.Printf("%d \n", i)       
 	fmt.Printf("%o \n", i)      
 	fmt.Printf("%x \n", 15)       
 	fmt.Printf("%X \n", 15)      
